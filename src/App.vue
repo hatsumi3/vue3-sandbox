304ad7f22d9db54096f7d47f67583bad65585fb1
hatsumi3 <al.usamiku39@gmail.com>
2023-05-07 15:42:42 +0900

    refactor: use script setup.

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,63 @@
 <template>
   <TheHeader text='My Counter' />
-  <div v-if="!validationMessageList.values.length">{{ count }}</div>
-  <div v-else v-for="message in validationMessageList.values" :key="message">
+  <div v-if="!validationMessageList.length">{{ count }}</div>
+  <div v-else v-for="message in validationMessageList" :key="message">
     {{ message }}
   </div>
   <BaseButton :disabled="hasMaxCount" @click="plusOne">+</BaseButton>
   <BaseButton :disabled="hasMinCount" @click="minusOne">-</BaseButton>
 
   <NumberInput v-model.numberOnly="inputCount" max="9999" min="0" />
+
   <BaseButton @onClick="insertCount">insert</BaseButton>
 </template>
 
-
-<script lang="ts">
-import { ref, computed, watch, defineComponent, reactive } from "vue";
+<script setup lang="ts">
 import TheHeader from './components/TheHeader.vue'
 import BaseButton from "./components/BaseButton.vue";
 import NumberInput from "./components/NumberInput.vue"
+import { ref, computed, watch } from 'vue';
 
-export default defineComponent({
-  components: {
-    TheHeader,
-    BaseButton,
-    NumberInput
-  },
-  setup() {
-    const count = ref(0)
-    const inputCount = ref(0)
-    const isEditing = ref(false)
+const count = ref(0);
+const inputCount = ref(0);
+const isEditing = ref(false);
 
-    const hasMaxCount = computed(() => {
-      return count.value >= 9999
-    })
-    const hasMinCount = computed(() => {
-      return count.value <= 0
-    })
-    const hasMaxInputCount = computed(() => {
-      return inputCount.value > 9999
-    })
-    const hasMinInputCount = computed(() => {
-      return inputCount.value < 0
-    })
-    const validationMessageList = computed(() => {
-      const validationList = reactive<String[]>([])
-      if (isEditing) {
-        validationList.push("編集中///")
-      }
-      if (hasMaxInputCount) {
-        validationList.push("9999///")
-      }
-      if (hasMinInputCount) {
-        validationList.push("0///")
-      }
-      return validationList
-    })
+const hasMaxCount = computed(() => count.value >= 9999);
+const hasMinCount = computed(() => count.value <= 0);
+const hasMaxInputCount = computed(() => inputCount.value > 9999);
+const hasMinInputCount = computed(() => inputCount.value < 0);
 
-    function plusOne() {
-      count.value++;
-    }
-    function minusOne() {
-      count.value--;
-    }
-    function insertCount() {
-      if (hasMaxInputCount.value || hasMinInputCount.value) return
-      count.value = inputCount.value;
-      isEditing.value = false
-    }
-    watch(inputCount, () => {
-      isEditing.value = true
-    })
-    return {
-      count,
-      inputCount,
-      isEditing,
-      hasMaxCount,
-      hasMinCount,
-      validationMessageList,
-      plusOne,
-      minusOne,
-      insertCount,
-    }
+const validationMessageList = computed(() => {
+  const validationList = []
+  if (isEditing.value) {
+    validationList.push("編集中///")
   }
-})
+  if (hasMaxInputCount.value) {
+    validationList.push("9999///")
+  }
+  if (hasMinInputCount.value) {
+    validationList.push("0///")
+  }
+  return validationList
+});
 
+watch(inputCount, () => {
+  isEditing.value = true;
+});
+
+function plusOne() {
+  count.value++;
+}
+
+function minusOne() {
+  count.value--;
+}
+
+function insertCount() {
+  if (hasMaxInputCount.value || hasMinInputCount.value) return
+  count.value = inputCount.value;
+  isEditing.value = false;
+}
 </script>
 
 <style scoped></style>
