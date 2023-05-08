7cbcb5e7740a86b0c13170bf2b0bd6346185d291
hatsumi3 <al.usamiku39@gmail.com>
2023-05-08 12:12:37 +0900

    feat: check lifecycle method.

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,17 @@
   <NumberInput v-model.numberOnly="inputCount" max="9999" min="0" />
 
   <BaseButton @onClick="insertCount">insert</BaseButton>
+
+  <br />
+  <button @click="toggleIsShow">isShow</button>
+  <div v-if="isShow">{{ text }}</div>
 </template>
 
 <script setup lang="ts">
 import TheHeader from './components/TheHeader.vue'
 import BaseButton from "./components/BaseButton.vue";
 import NumberInput from "./components/NumberInput.vue"
-import { ref, computed, watch } from 'vue';
+import { ref, computed, watch, onBeforeMount, onUpdated, onBeforeUnmount, onUnmounted, onBeforeUpdate, onMounted, provide } from 'vue';
 
 const count = ref(0);
 const inputCount = ref(0);
@@ -58,6 +62,46 @@ function insertCount() {
   count.value = inputCount.value;
   isEditing.value = false;
 }
+
+// life-cycle hook
+const isShow = ref(true);
+const text = ref("")
+
+function toggleIsShow() {
+  isShow.value = !isShow.value
+}
+
+onBeforeMount(() => {
+  console.log("on before mount")
+})
+
+onMounted(() => {
+  console.log("on mount")
+})
+
+onBeforeUpdate(() => {
+  console.log("on before update", text.value)
+})
+
+onUpdated(() => {
+  console.log("on update", text.value)
+})
+
+onBeforeUnmount(() => {
+  console.log("on before unmounted")
+})
+
+onUnmounted(() => {
+  console.log("on unmounted")
+})
+
+console.log("on created")
+
+
+// inject-provide
+provide("unique", "My Counter 2")
+
+
 </script>
 
 <style scoped></style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,22 +2,24 @@
     <header> {{ text }}</header>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { inject } from 'vue';
 
-export default defineComponent({
-    props: {
-        text: {
-            text: String,
-            default: 'abc',
-            required: true,
-            validator: (value: String) => {
-                // 空白orNullorUndefinedはfalse
-                return !!value
-            },
-        }
-    },
-})
+defineProps({
+    text: {
+        text: String,
+        default: 'abc',
+        required: true,
+        validator: (value: String) => {
+            // 空白orNullorUndefinedはfalse
+            return !!value
+        },
+    }
+});
+
+const title = inject("unique")
+console.log(title)
+
 
 </script>
 
